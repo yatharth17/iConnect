c3930202ab0de99f97587340b193b6418619045e
yatharth17 <yatharthkatyal4@gmail.com>
2021-07-04 14:24:46 +0530

    setup

---
+++ src/Imessage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Imessage.css'
+import SideBar from './SideBar'
+
+function Imessage() {
+    return (
+        <div className="imessage">
+            {/*SideBar */}
+            <SideBar/>
+            {/* Chat */}
+        </div>
+    )
+}
+
+export default Imessage
---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SideBar.css'
+
+function SideBar() {
+    return (
+        <div>
+           This is the SideBar 
+        </div>
+    )
+}
+
+export default SideBar
